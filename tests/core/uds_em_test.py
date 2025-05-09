8fa49ab847448945a556d9994a3f56141bf671f6
Mateusz Krajewski <matikrajek42@gmail.com>
2025-05-09 10:04:55 +0200

    fix ut

--- tests/core/uds_em_test.py
+++ tests/core/uds_em_test.py
@@ -4,6 +4,29 @@ import time
 DID_ID = "0004"
 DEVICE_IP = "192.168.10.101"
 
+
+
+
+
+import paramiko
+
+def restart_using_ssh():
+    host = "192.168.10.101"
+    username = "root"
+    password = ""
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(host, username=username, password=password)
+        stdin, stdout, stderr = client.exec_command("reboot")
+        client.close()
+    except:
+        assert False
+    assert True
+
+
+
+
 @tools.pytest.pytest_main.pytest_register_test
 def uds_machine_em_did_read_test():
     client = UdsClient(DEVICE_IP)
@@ -17,10 +40,12 @@ def uds_machine_em_did_read_test():
     res = client.WriteDiD("0003", "2e6c")
     # assert res
     assert res[0] == 0x6e
-    time.sleep(5)
+    time.sleep(2)
     res = client.ReadDiD(DID_ID)
     assert res[0] == 0x62
     assert res[1] == 0x00
     assert res[2] == 0x04
     assert res[3] == 0x03
     assert res[12] == 0x04
+    restart_using_ssh()
+    time.sleep(20)
