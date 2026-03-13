93f8b135ff283050320ad9cc1c37d509e81eedc9
Bartosz Snieg <snieg45@gmail.com>
2026-03-13 19:49:38 +0100

    Adding maos support

--- tools/common/connect_tar.sh
+++ tools/common/connect_tar.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-tar -cf $1 -T /dev/null
+# Wykrywanie właściwej wersji tar
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    TAR_BIN="/opt/homebrew/bin/gtar"
+    if [ ! -f "$TAR_BIN" ]; then
+        echo "Błąd: Plik $TAR_BIN nie istnieje. Zainstaluj gnu-tar (brew install gnu-tar)."
+        exit 1
+    fi
+else
+    TAR_BIN="tar"
+fi
 
+# Tworzy pusty plik tar
+$TAR_BIN -cf "$1" -T /dev/null
+
+# Łączenie plików
 for i in "${@:2}"; do
-    tar --concatenate --file=$1 $i
+    $TAR_BIN --concatenate --file="$1" "$i"
 done
